165d00c35683aa29eb041a10d2af601fdd9a924b
ramiro-uziel <ramirouziel15@gmail.com>
2025-02-06 01:12:43 -0600

    Bob lib expose to repo

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,9 @@
+import { ViewStyle } from 'react-native';
+type BlurViewProps = {
+    style: ViewStyle;
+    gradientMask?: string;
+    maxBlurRadius?: number;
+};
+export declare const VariableBlurView: import("react-native").HostComponent<BlurViewProps> | (() => never);
+export {};
+//# sourceMappingURL=index.d.ts.map
